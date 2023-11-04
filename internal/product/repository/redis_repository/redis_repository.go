372ee469eaec5dd38ff2a8b12acbb34ec6bf60e5
DyingFirst <v.artyo@mail.ru>
2023-11-04 19:24:22 +0300

    edit: delete logger

--- internal/product/repository/redis_repository/redis_repository.go
+++ internal/product/repository/redis_repository/redis_repository.go
@@ -4,17 +4,15 @@ import (
 	"URLShorter/internal/product"
 	"context"
 	"github.com/redis/go-redis/v9"
-	"github.com/sirupsen/logrus"
 	"time"
 )
 
 type RedisRepo struct {
-	logger *logrus.Logger
-	rdb    *redis.Client
+	rdb *redis.Client
 }
 
-func NewRedisRepo(logger *logrus.Logger, rdb *redis.Client) product.RedisRepo {
-	return &RedisRepo{logger: logger, rdb: rdb}
+func NewRedisRepo(rdb *redis.Client) product.RedisRepo {
+	return &RedisRepo{rdb: rdb}
 }
 
 func (r *RedisRepo) NewValue(ctx context.Context, ShortURL string, OriginalURL string) error {
